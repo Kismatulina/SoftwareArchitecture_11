04f03d40264a2294476c5613a7040f4f90cd9d24
user <user@WIN-HB4PQ0MH1T0>
2025-09-14 17:29:15 +0300

    Добавьте файлы проекта.

---
+++ ClinicService/Services/IClientRepository.cs
@@ -0,0 +1,11 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services
+{
+    /// <summary>
+    /// Конкретный тип интерфейса репозитория для работы со справочником клиентов в БД
+    /// </summary>
+    public interface IClientRepository : IRepository<Client, int>
+    {
+    }
+}
---
+++ ClinicService/Services/IRepository.cs
@@ -0,0 +1,53 @@
+﻿using ClinicService.Models;
+using System.Security.Cryptography; //добавили
+/// <summary>
+/// Обобщенный интерфейс репозиторий для работы с БД по записям
+/// </summary>
+/// <typeparam name="T">Класс записи (клиент, животное, запись на консультацию)</typeparam>
+/// <typeparam name="TId">Идентификатор записи (клиент, животное, запись на консультацию)</typeparam>
+namespace ClinicService.Services
+{
+    public interface IRepository<T, TId> // T, TId обобщенный параметр
+    {
+        int Create(T item);//регистрируем клиента в нашей системе
+        int Update(T item);
+
+        int Delete(TId id);
+
+        T GetById(TId id);
+        List<T> GetAll();
+    }
+}
+
+/// <summary>
+/// Создание записи (клиент, животное, запись на консультацию)
+/// </summary>
+/// <param name="item">Переданные данные для записи</param>
+/// <returns>Результат создания записи в БД</returns>
+
+
+/// <summary>
+/// Редактирование записи (клиент, животное, запись на консультацию)
+/// </summary>
+/// <param name="item">Переданные данные для изменения записи</param>
+/// <returns>Результат изменение записи в БД</returns>
+
+
+/// <summary>
+/// Удаление записи по ID
+/// </summary>
+/// <param name="id">Идентификатор записи в БД (клиент, животное, запись на консультацию)</param>
+/// <returns>Результат удаления записи из БД</returns>
+
+
+/// <summary>
+/// Получение записи из БД по ID (клиент, животное, запись на консультацию)
+/// </summary>
+/// <param name="id">Идентификатор записи в БД (клиент, животное, запись на консультацию)</param>
+/// <returns>Информацию из БД по запрошенной записи</returns>
+
+
+/// <summary>
+/// Получение всех записей в программе
+/// </summary>
+/// <returns>Всех записей по справочнику класса записи</returns>
